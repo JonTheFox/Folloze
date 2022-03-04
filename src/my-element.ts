df47375055cd7d39874a6dacf8f30fb241a69471
jonthefox <weissjon@gmail.com>
2022-03-04 20:32:46 +0200

    write the autocomplete logic

--- src/my-element.ts
+++ src/my-element.ts
@@ -31,6 +31,7 @@ export class MyElement extends LitElement {
 
   constructor() {
     super();
+      debugger;
     this._eventReferences = {};
     this._matches = [];
     this.items = [];
@@ -38,14 +39,20 @@ export class MyElement extends LitElement {
     this.maxSuggestions = MAX_MATCHES;
   }
 
+  //
+  // methods
+  //
+
   // called after the element's DOM has been updated the first time,
   // immediately before the "updated" method is called.
   firstUpdated() {
+    debugger;
     this.suggestionEl = this.shadowRoot?.getElementById('suggestions');
     this.suggestionEl.style.width =
       this.contentElements.getBoundingClientRect().width + 'px';
 
     // add our event handlers, found in the _eventReferences field, to keyboard events
+    // this will allow us to remove them later
     this._eventReferences.onFocus = this._onFocus.bind(this);
     this._eventReferences.onBlur = this._onBlur.bind(this);
     this._eventReferences.onKeyDown = this._onKeyDown.bind(this);
@@ -72,13 +79,14 @@ export class MyElement extends LitElement {
 
   //called when the element's DOM has been updated and rendered. We implement this to perform some tasks after an update.
   updated(changed) {
+     debugger;
     // if the dropdown is open and we have a list of suggestions
     if (
       changed.has('opened') &&
       this.opened &&
       this._suggestionEl.childElementCount
     ) {
-      // remove the 'active' class from the suggestions (they might have been reviously-assigned this class)
+      // remove the 'active' class from the suggestions (they might have been ssigned this class previously)
       for (let item of this._suggestionEl.children) {
         item.classList.remove('active');
       }
@@ -88,7 +96,190 @@ export class MyElement extends LitElement {
     }
   }
 
+  // invoked when a component is removed from the document's DOM
+  disconnectedCallback() {
+     debugger;
+    if (!this.contentElement) {
+      // no events to remove
+      return;
+    }
+
+    //remove our event listeners
+    this.contentElement.removeEventListener(
+      'keydown',
+      this._eventReferences.onKeyDown
+    );
+    this.contentElement.removeEventListener(
+      'keyup',
+      this._eventReferences.onKeyUp
+    );
+    this.contentElement.removeEventListener(
+      'focus',
+      this._eventReferences.onFocus
+    );
+    this.contentElement.removeEventListener(
+      'blur',
+      this._eventReferences.onBlur
+    );
+  }
+
+  _onKeyDown(ev) {
+     debugger;
+    if (ev.key === 'ArrowUp' || ev.key === 'ArrowDown') {
+      // tell the user agent that if the event does not get excplicitly handled, its default value will not be taken as it as normally would be.
+      ev.preventDefault();
+      // prevent further propagating the current event in the capturing and bubbling phases.
+      ev.stopPropagation();
+    }
+  }
+
+  _onKeyUp(ev) {
+     debugger;
+    switch (ev.key) {
+      case 'ArrowUp':
+        ev.preventDefault();
+        ev.stopPropagation();
+        // highlight the previous element of the suggestion list
+        this._markPreviousElement();
+        break;
+      case 'ArrowDown':
+        ev.preventDefault();
+        ev.stopPropagation();
+        // highlight the following element of the suggestion list
+        this._markPreviousElement();
+        break;
+      case 'Enter':
+        // if there is a highlighted element, invoke a click on it
+        this._highlightedEl && this._highlightedEl.click();
+        break;
+
+      default:
+        if (this.items.length) {
+          let suggestions = [];
+          let value = this.contentElement.value;
+
+          suggestions =
+            value &&
+            this.items
+              .filter((item) => {
+                return item.text
+                  .replace(',', '') // remove commas
+                  .replace(/\s/g, '') // remove whitespaces
+                  .toLowerCase()
+                  .search(
+                    value.replace(',', '').replace(/\s/g, '').toLowerCase()
+                  );
+              })
+              .slice(0, this.maxSuggestions); // limit results
+          if (suggestions.length === 0) {
+            suggestions = [];
+            suggestions.push({value: null, text: 'Sorry, no matches'});
+          }
+
+          this.suggest(suggestions);
+        }
+        break;
+    }
+  }
+
+  _markPreviousElement() {
+     debugger;
+    if (!this.highlightedEl || !this._highlightedEl.previousElementSibling) {
+      return;
+    }
+
+    // remove the 'active' class from the currently-active element
+    this._highligtedEl.classList.remove('active');
+    // set the previous sibling element to be the now-highlighted element
+    this._highligtedEl = this._highlightedEl.previousElementSibling;
+    this._highlightedEl.classList.add('active');
+  }
+
+  _markNextElement() {
+     debugger;
+    if (!this.highlightedEl || !this._highlightedEl.nextElementSibling) {
+      return;
+    }
+
+    // remove the 'active' class from the currently-active element
+    this._highligtedEl.classList.remove('active');
+    // set the next sibling element to be the now-highlighted element
+    this._highligtedEl = this._highlightedEl.nextElementSibling;
+    this._highlightedEl.classList.add('active');
+  }
+
+  _onFocus(ev) {
+     debugger;
+    this._blur = false;
+    // if there are matches, open the dropdown to show them
+    this._matches.length && this.open();
+  }
+
+  _onBlur(ev) {
+     debugger;
+    this._blur = true;
+    // if the component lost focus and the mouse cursor is not inside the component,
+    // close the dropdown
+    !this._mouseEnter && this.close();
+  }
+
+  _handleItemMouseEnter(ev) {
+     debugger;
+    this._mouseEnter = true;
+  }
+
+  _handleItemMouseLeave(ev) {
+     debugger;
+    this._mouseEnter = false;
+    // if the mouse's cursor leaves the component and doesn't have focus,
+    // wait 500 milliseconds and then close the dropdown
+    this._blur && setTimeout((_) => this.close(), 500);
+  }
+
+  open() {
+     debugger;
+    console.log('open()');
+    // open only if there are matches to show
+    if (!this._matches.length) return;
+    this.opened = true;
+  }
+
+  close() {
+     debugger;
+    console.log('close()');
+    // hide suggesting list dropdown
+    this.opened = false;
+    // reset the highlighted element to be null
+    this._highlightedEl = null;
+  }
+
+  suggest(suggestions) {
+     debugger;
+    console.log('suggest()');
+    this._matches = suggestions || {};
+    // if there are matches, open the dropdown to show the suggestions. Else, close the dropdown.
+    this._matches.length ? this.open() : this.close();
+  }
+
+  autocomplete(value, text) {
+     debugger;
+    // called when selecting a suggestion
+    this.contentElement.value = value;
+    this.close();
+
+    // propagate the 'selected-autocomplete' event to all subscribers of this event.
+    this.dispatchEvent(
+      new CustomEvent('selected-autocomplete', {
+        detail: {value, text}, // data to pass to subsribers
+        composed: true,
+        bubbles: true, // so that parent subscribers can catch this event and handle it
+      })
+    );
+  }
+
+  //
   // getters
+  //
   get contentElement() {
     //  if the _inputEl node exists
     if (this._inputEl) {
@@ -124,27 +315,77 @@ export class MyElement extends LitElement {
   count = 0;
 
   override render() {
+    // Use lit-html to render the element template
+    // we must implement the render method for any component that extends the LitElement base class.
     return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
+      <style>
+        ul {
+          position: absolute;
+          margin: 0;
+          padding: 0;
+          z-index: 5000;
+          background: white;
+          display: block;
+          list-style-type: none;
+          width: 100% !important;
+          border: 1px solid black;
+        }
+
+        li {
+          padding: 10px;
+        }
+
+        li.active {
+          background: gray;
+        }
+
+        [hidden] {
+          display: none;
+        }
+      </style>
+
+      <slot id="dropdown-input">
+        <input id="defaultInput" type="text" />
+      </slot>
+
+      <ul 
+        id="suggestions"
+        ?hidden=${!this.opened)}
+        @mouseenter=${this._handleItemMouseEnter}
+        @mouseleave=${this._handleItemMouseLeave}
+      >
+
+        ${this._matches.map(item=>{
+          return html`
+            <li @click=${ev=>{
+              this.autocomplete(item.text, item.value ? item.value : null);
+            }}>
+            ${item.text}
+            </li>
+          `;
+        })}
+
+      </ul>
     `;
+    
   }
 
-  private _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
-  }
+  debugger;
 
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
-  }
-}
+  window.customElements.define("lit-autocomplete", litAutocomplete);
 
-declare global {
-  interface HTMLElementTagNameMap {
-    'my-element': MyElement;
-  }
-}
+//   private _onClick() {
+//     this.count++;
+//     this.dispatchEvent(new CustomEvent('count-changed'));
+//   }
+
+//   sayHello(name: string): string {
+//     return `Hello, ${name}`;
+//   }
+// }
+
+// declare global {
+//   interface HTMLElementTagNameMap {
+//     'my-element': MyElement;
+//   }
+// }
